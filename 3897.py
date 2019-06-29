07a08b190439fdfad2523ca2bd8fce7e09608bf1
lucianobajr <45442173+lucianobajr@users.noreply.github.com>
2019-06-29 20:55:11 -0300

    Add files via upload

---
+++ 3897.py
@@ -0,0 +1,12 @@
+import itertools
+casos = []
+time1,time2=map(str,input().split(','))
+gol1=int(input())
+gol2=int(input())
+teste = time1+time2
+if gol1 == 0 and gol2 == 0:
+    print(0)
+else:
+    casos=sorted(set(''.join(anagrama) for anagrama in itertools.product(teste,repeat=(gol1+gol2))if(anagrama.count(time1)==gol1 and anagrama.count(time2)==gol2)))
+    for i in range(len(casos)):
+        print(casos[i])
